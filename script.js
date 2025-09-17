dd1c4b70e71abc865c49e79de75ad7dc67c26e52
Carolina Oldertz <carolina@Carolinas-MacBook-Air.local>
2025-09-17 13:55:53 +0200

    added products, a hamburger menu and changed some styling

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const hamMenu = document.getElementById("hamMenu")
 const hamMenuLinks = document.getElementById("hamMenuLinks")
 
 hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active")
   hamMenuLinks.classList.toggle("show")
 })
+
